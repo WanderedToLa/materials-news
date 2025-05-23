829628c1287da1f563ed1a755fe8b1f0fc9b953b
Wooseok <rladntjr9808@gmail.com>
2025-05-23 21:30:31 +0900

    test react globe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+"use client";
+
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+
+const Globe = dynamic(() => import("react-globe.gl"), { ssr: false });
+
+export default function Page() {
+  const [data, setData] = useState<{ lat: number; lng: number }[]>([]);
+
+  useEffect(() => {
+    setData([
+      { lat: 37.5665, lng: 126.978 }, // Seoul
+      { lat: 40.7128, lng: -74.006 }, // New York
+      { lat: 51.5074, lng: -0.1278 }, // London
+    ]);
+  }, []);
+
+  return (
+    <div className="w-full h-screen">
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        pointsData={data}
+        pointAltitude={0.01}
+        pointColor={() => "orange"}
+      />
+    </div>
+  );
 }
